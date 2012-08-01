ca3c4fae5def617dda0ff10a0587248c89165bd8
Nicola Segata <nicola.segata@unitn.it>
2012-08-01 11:55:27 -0400

    added conv_ctxt2rxl.py added conv_ctxt2ttxt.py changed merg_ctxt.py

---
+++ conv_ctxt2ttxt.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import sys
+import collections
+
+try:
+    import argparse as ap
+    import bz2 
+except ImportError:
+    sys.stderr.write( "argparse not found" )
+    sys.exit(-1)
+
+def read_params( args ):
+    p = ap.ArgumentParser(description='Convert core gene txt file'
+            ' substituting gene IDs with genomes IDs\n')
+
+    p.add_argument( 'ctxt', nargs='?', default=None, type=str,
+            help=   "the input ctxt file [stdin if not present]")
+    p.add_argument('--g2t', metavar="Mapping file from genes to taxa",
+            default=None, type=str )
+    p.add_argument('--t2g', metavar="Mapping file from taxa to genes",
+            default=None, type=str )
+    p.add_argument('txt', nargs='?', default=None, type=str,
+            help=   "the output gtxt file, compressed if fiven with bz2 extension\n"
+                    "[stdout if not present]")
+
+    return vars( p.parse_args() )
+
+if __name__ == "__main__":
+    args = read_params( sys.argv )
+    uc2cl = collections.defaultdict( set )
+
+    openr = bz2.BZ2File if args['ctxt'] and args['ctxt'].endswith(".bz2") else open
+    with (openr(args['ctxt']) if args['ctxt'] else sys.stdin) as inp:
+        valin = [[int(a) for a in l.strip().split('\t')] for l in inp]
+    if not args['g2t'] and not args['t2g']:
+        sys.stdout.write("Error one of --t2g and --g2t must be provided\n")
+        sys.eit(0)
+    g2t = {}
+
+
+    if args['g2t']:
+        with open( args['g2t'] ) as inp:
+            g2t = dict(([int(a) for a in l.strip().split('\t')] for l in inp))
+    elif args['t2g']:
+        with open( args['g2t'] ) as inp:
+            for ll in (l.strip().split('\t') for l in inp):
+                for g in ll[1:]:
+                    g2t[int(g)] = int(ll[0])
+    
+    #valout = [[g2t[k]]+[g2t[vv] for vv in v] for k,v in valin]
+    for j,v in enumerate(valin):
+        #for i,vv in enumerate(v):
+        #    vn = set(g2t[vv] for vv in v)
+        valin[j] = set(g2t[vv] for vv in v)
+
+    openw = bz2.BZ2File if args['txt'] and args['txt'].endswith(".bz2") else open
+    with (openw(args['txt'],"w") if args['txt'] else sys.stdout) as out:
+        for v in sorted(valin,key=lambda x:-len(x)):
+            out.write( "\t".join([str(s) for s in v]) +"\n" )
--- merg_ctxt.py
+++ merg_ctxt.py
@@ -14,13 +14,13 @@ def read_params( args ):
     p = ap.ArgumentParser(description="Merge two core txt files"
             "where the second are subclusters of the first" )
 
-    p.add_argument( 'ctxt1', nargs='?', default=None, type=str,
+    p.add_argument( 'ctxt1', default=None, type=str,
             help=   "the first ctxt")
-    p.add_argument('ctxt2', nargs='?', default=None, type=str,
-            help=   "the second ctxt")
-    p.add_argument('out_ctxt', nargs='?', default=None, type=str,
+    p.add_argument('--out_ctxt', default=None, type=str,
             help=   "the output txt file compressef if given with bz2 extension\n"
                     "[stdout if not present]")
+    p.add_argument('ctxt2', nargs='*', default=None, type=str,
+            help=   "the second ctxt")
 
     return vars( p.parse_args() )
 
@@ -28,22 +28,24 @@ if __name__ == "__main__":
     args = read_params( sys.argv )
     uc2cl = collections.defaultdict( set )
 
+    print args['ctxt1']
+    print args['ctxt2']
+
     openr = bz2.BZ2File if args['ctxt1'].endswith(".bz2") else open
-    ctxt1 = dict([v[0],v[1:] if len(v) > 1 else []) 
-                for v in (l.strip().split('\t') for l in openr(args['ctxt1']))])
-    openr = bz2.BZ2File if args['ctxt2'].endswith(".bz2") else open
-    ctxt2 = dict([v[0],v[1:] if len(v) > 1 else []) 
-                for v in (l.strip().split('\t') for l in openr(args['ctxt2']))])
+    mtxt = dict([([v[0],(v[1:] if len(v) > 1 else [])]) for v in (l.strip().split('\t') for l in openr(args['ctxt1']))])
+    
+    for f in args['ctxt2']:
+        openr = bz2.BZ2File if f.endswith(".bz2") else open
+        ctxt = dict([([v[0],(v[1:] if len(v) > 1 else [])]) for v in (l.strip().split('\t') for l in openr(f))])
 
-    mtxt = dict()
-    for k,v in ctxt1.items():
-        nv = v
-        for kk in [k]+v:
-            if kk in ctxt2:
-                nv += ctxt2[kk]
-        mtxt[k] = nv
+        for k,v in mtxt.items():
+            nv = v
+            for kk in [k]+v:
+                if kk in ctxt:
+                    nv += ctxt[kk]
+            mtxt[k] = nv
 
-    openw = bz2.BZ2File if args['txt'].endswith(".bz2") else open
-    with (openw(args['txt'],"w") if args['txt'] else sys.stdout) as out:
+    openw = bz2.BZ2File if args['out_ctxt'].endswith(".bz2") else open
+    with (openw(args['out_ctxt'],"w") if args['out_ctxt'] else sys.stdout) as out:
         for k,v in sorted(mtxt.items(),key=lambda x:-len(x[1])):
             out.write( "\t".join([k]+list(v)) +"\n" )
