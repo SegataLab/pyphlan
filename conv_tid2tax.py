8ad292dd32c39b10f3612a5de9b5df6da28e274a
Nicola Segata <nicola.segata@unitn.it>
2012-08-06 22:28:51 -0400

    added conv_tid2tax.py changed fna_sss.py changed pyphlan.py changed utils.py

---
+++ conv_tid2tax.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import sys
+import collections
+import utils
+import pandas
+import StringIO
+
+try:
+    import argparse as ap
+    import bz2 
+except ImportError:
+    sys.stderr.write( "argparse not found" )
+    sys.exit(-1)
+
+def read_params( args ):
+    p = ap.ArgumentParser(description='Generate a taxonomic txt hierarchy'
+            ' with genome assignments from IMG taxa table\n')
+
+    p.add_argument( 'img', nargs='?', default=None, type=str,
+            help=   "the input uc file [stdin if not present]")
+    p.add_argument('txt', nargs='?', default=None, type=str,
+            help=   "the output txt file compressed if fiven with bz2 extension\n"
+                    "[stdout if not present]")
+
+    return vars( p.parse_args() )
+
+qm = "?"
+def get_qm( s ):
+    if s is None:
+        return qm
+    if not s:
+        return qm
+    if type(s) != str:
+        return qm
+    if s in ['Unclassified','unclassified']:
+        return qm
+    if s in ['sp','sp.','Sp','Sp.','spp','spp.','Spp','Spp.']:
+        return qm
+    return s.replace("Candidatus ","").replace(" ","_").replace(".","_")
+
+
+if __name__ == "__main__":
+    args = read_params( sys.argv )
+    uc2cl = collections.defaultdict( set )
+
+    tax_lev = "dpcofgs"
+    tax_lev_exp = ['Domain','Phylum','Class','Order','Family','Genus','Species']
+
+    with utils.openr(args['img'],"rU") as inp:
+        table = pandas.read_table( inp, sep='\t', index_col=0)
+   
+    table = table[table['Domain'].isin(['Bacteria','Archaea'])]
+    table = table[table['Gene Count'] > 250.0]
+    table = table[table['Genome Size'] > 50000.0]
+   
+    table = table.reindex(columns=tax_lev_exp+['Genome Name'])
+
+    with utils.openw(args['txt']) as out:
+        for i,t in table.iterrows():
+            out.write( "\t".join( [ #str(-int(i)),
+                                  ".".join( ["__".join([taxl, get_qm(t[taxle])]) 
+                                      for taxl,taxle in  zip(tax_lev,tax_lev_exp)] + ["t__"+str(-int(i))]
+                                        ),
+                                  #str(t['Genome Name'])
+                                  ] ) + "\n")
+    
+    
