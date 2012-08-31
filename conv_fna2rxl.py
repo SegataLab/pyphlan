4b0d334b56c4f24b19c5b71a6487faf53bac2516
Nicola Segata <nicola.segata@unitn.it>
2012-08-31 08:43:19 -0400

    added choco_summary.py changed conv_fna2rxl.py

--- conv_fna2rxl.py
+++ conv_fna2rxl.py
@@ -42,4 +42,4 @@ if __name__ == "__main__":
         out.write( str(len(fna))+" "+str(n)+"\n" )
 
         for k,v in fna.items():
-            out.write( str(k)[1:]+" "*(15-len(str(k)[1:]))+str(v.seq) +"\n" )
+            out.write( str(k)+" "*(15-len(str(k)[1:]))+str(v.seq) +"\n" )
