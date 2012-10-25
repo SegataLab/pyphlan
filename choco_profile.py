283586d88d88d775380aa109b40453c80df02998
Nicola Segata <nicola.segata@unitn.it>
2012-10-25 14:49:22 -0400

    changed choco_profile.py

--- choco_profile.py
+++ choco_profile.py
@@ -38,6 +38,9 @@ if __name__ == "__main__":
     clades2terms = ppa.clades2terms( tree.tree ) 
 
     clades2taxa = dict([(clade.full_name,set([-int(taxon.name[4:]) for taxon in taxa])) for clade,taxa in clades2terms.items()])
+    ntaxa = 0
+    for v in clades2terms.values():
+        ntaxa += len(v)
 
     for l in open( args['cscores'] ):
         gene_seed, clade, n, n_tot, coreness = l.strip().split('\t')
@@ -54,7 +57,7 @@ if __name__ == "__main__":
         if len(line) < 2:
             continue
         fr,to = line
-        fr,to = int(fr.split("_")[0]), int(to) # can be improved!!!!
+        fr,to = int(fr.split("_")[0]), to # can be improved!!!!
         maps[fr].add( to )
 
     g2t,g2c,c2g = {},{},{}
@@ -74,9 +77,10 @@ if __name__ == "__main__":
             line = list(l.strip().split('\t'))
             #if int(line[0]) not in genomes:
             #    continue
-            vals = [int(a) for a in line if utils.is_number(a)]
+            #vals = [int(a) for a in line if utils.is_number(a)]
+            vals = [a for a in line]
             if len(vals) > 1:
-                g2c[vals[0]] = vals[1:]
+                g2c[int(vals[0])] = vals[1:]
     for g,c in g2c.items():
         for cc in c:
             c2g[cc] = g
@@ -92,7 +96,7 @@ if __name__ == "__main__":
                 taxa_id, clade, n, n_tot, coreness, n_ext_seeds, n_ext_taxa, uniqueness = fwmarkers[gene_seed]
                 if uniqueness < 0.01:
                      out.write( "\t".join(["FWMARKER",str(gene_seed),str(taxa),clade,str(n), str(n_tot), str(coreness),
-                                                      str(n_ext_seeds), str(n_ext_taxa), str(uniqueness)]) +"\n" ) 
+                                                      str(n_ext_seeds), str(n_ext_taxa), str(1.0-uniqueness)]) +"\n" ) 
 
                 if gene_seed in maps:
                     ext_tax = set([(c2g[s] if s in c2g else 0) for s in maps[gene_seed]])
@@ -100,11 +104,12 @@ if __name__ == "__main__":
                     ext_tax_ko = ext_tax - clades2taxa[clade]
                     ext_tax_okl = ":".join([str(s) for s in ext_tax_ok])
                     ext_tax_kol = ":".join([str(s) for s in ext_tax_ko])
+                    muniq = 1.0 - float(len(ext_tax_kol)) / float(ntaxa)
 
                     out.write( "\t".join(["MARKER",str(gene_seed),str(taxa),clade,str(n), str(n_tot), str(coreness),
-                                                   str(n_ext_seeds), str(n_ext_taxa), str(uniqueness), 
+                                                   str(n_ext_seeds), str(n_ext_taxa), str(1.0-uniqueness), 
                                                    str(len(ext_tax)), str(len(ext_tax_ok)), str(len(ext_tax_ko)), 
-                                                   ext_tax_okl, ext_tax_kol]) +"\n" ) 
+                                                   str(muniq), ext_tax_okl, ext_tax_kol]) +"\n" ) 
 
     """
     gid2cores = collections.defaultdict( set )
---
+++ fna_exact_matches.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python 
+from __future__ import with_statement
+
+import sys
+import argparse
+import os
+import textwrap
+from collections import namedtuple as nt
+import random as rnd
+rnd.seed(1982)
+import utils
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
+def read_params(args):
+    parser = argparse.ArgumentParser(description='Split/Select/Randomize/Subsample a multi fasta file')
+    arg = parser.add_argument
+    arg( 'inp_f', metavar='INPUT_FILE', nargs='?', default=None, type=str,
+         help="the input fna file [stdin if not present]")
+    arg( 'out_f', metavar='OUTPUT_FILE', nargs='?', default=None, type=str,
+         help="the output fna file [stdout if not present]")
+
+    parser.add_argument('-s', metavar='Subsequene to look for', required = True, type = str )
+
+    return vars(parser.parse_args())
+
+if __name__ == '__main__':
+    par = read_params(sys.argv)
+
+    ss = par['s'].lower
+    f = os.path.basename(par['inp_f']).split(".")[0]
+    with utils.openw( par['out_f'] ) as outf:
+        for r in SeqIO.parse( utils.openr(par['inp_f']), "fasta"):
+            if ss in r.seq.lower:
+                outf.write( f + "\t" + str(r.id) + "\n" )
