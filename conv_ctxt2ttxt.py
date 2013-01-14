c2a9433ab6a7e2076daa390bd3eb7dd9229af546
nsegata <nsegata@localhost>
2013-01-14 08:19:21 +0100

    branch 'default' subrepo phylophlan subrepo sfle subrepo pyphlan changed .hgsubstate changed 1_phylophlan_taxonomic_curation.sh changed 3_species_clusters.sh changed 4_highlev_clusters.sh changed 5_highlev_unique_clusters.sh changed 6_marker.sh changed 7_viruses.sh changed 8_eukaryotes.sh changed input/microbes.tax.txt changed input/taxontable.txt changed input/viruses.tax.txt changed scons.py removed 4_markers.sh

--- conv_ctxt2ttxt.py
+++ conv_ctxt2ttxt.py
@@ -30,6 +30,7 @@ if __name__ == "__main__":
     args = read_params( sys.argv )
     uc2cl = collections.defaultdict( set )
 
+    """
     with utils.openr( args['ctxt'] ) as inp:
         valin = [[int(a) for a in l.strip().split('\t')] for l in inp]
     if not args['g2t'] and not args['t2g']:
@@ -56,3 +57,38 @@ if __name__ == "__main__":
     with utils.openw(args['txt']) as out:
         for v in sorted(valin,key=lambda x:-len(x)):
             out.write( "\t".join([str(s) for s in v]) +"\n" )
+
+    """
+
+    if not args['g2t'] and not args['t2g']:
+        sys.stdout.write("Error one of --t2g and --g2t must be provided\n")
+        sys.exit(0)
+    g2t = {}
+
+
+    if args['g2t']:
+        with utils.openr( args['g2t'] ) as inp:
+            #g2t = dict(([int(a) for a in l.strip().split('\t')] for l in inp))
+            for l in inp:
+                f,t = l.strip().split('\t')
+                g2t[int(f)] = int(t)
+    elif args['t2g']:
+        with utils.openr( args['t2g'] ) as inp:
+            for ll in (l.strip().split('\t') for l in inp):
+                for g in ll[1:]:
+                    g2t[int(g)] = int(ll[0])
+    
+    #for v in valin:
+    #    valin[j] = [v[0]]+list(set(g2t[vv] for vv in v))
+
+    with utils.openw(args['txt']) as out:
+        with utils.openr( args['ctxt'] ) as inp:
+            for l in inp:
+                valin = [int(a) for a in l.strip().split('\t')]
+                valin = [valin[0]]+list(set(g2t[vv] for vv in valin)) 
+    
+                #for v in sorted(valin,key=lambda x:-len(x)):
+                out.write( "\t".join([str(s) for s in valin]) +"\n" )
+
+
+
