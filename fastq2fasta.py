284cc6c57b3d51272585171108e97cdf280166a9
duytin <17859735+duytintruong@users.noreply.github.com>
2014-07-23 16:08:31 +0200

    add fastq2fasta.py

---
+++ fastq2fasta.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from Bio import SeqIO
+import argparse as ap
+import sys
+
+def read_params(args):
+	p = ap.ArgumentParser(description = 'fastq2fasta.py Parameters\n')
+	p.add_argument('--ifn', required = True, default = None, type = str)
+	p.add_argument('--ofn', required = True, default = None, type = str)
+	return vars(p.parse_args())
+	
+if __name__ == '__main__':
+	args = read_params(sys.argv)
+	with open(ofn, 'w') as ofile:
+		for r in SeqIO.parse(ifn, "fastq"):
+			SeqIO.write(r, ofile, "fasta")
