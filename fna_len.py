c2012dd60207589402937afbd5cd1b10f6540229
edoardo pasolli <19696889+edoardopasolli@users.noreply.github.com>
2014-10-30 13:43:43 +0100

    Length statistics

--- fna_len.py
+++ fna_len.py
@@ -11,6 +11,7 @@ rnd.seed(1982)
 import utils
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
+import numpy as np
 
 def read_params(args):
     parser = argparse.ArgumentParser(description='Display the length of each fasta entry')
@@ -21,19 +22,28 @@ def read_params(args):
          help="the output txt file [stdout if not present]")
     arg( '-q', action='store_true', 
          help="set this for fastq")
-    arg( '-t','--total', action='store_true', help="Print only the sum of the length of all sequences\n") 
+    arg( '-t','--total', action='store_true', help="Print only the sum of the length of all sequences\n")
+    arg( '-s','--stat', action='store_true', help="Print only the statistics about the length of sequences\n")
     return vars(parser.parse_args())
 
 if __name__ == '__main__':
     par = read_params(sys.argv)
 
     ltot = 0
+    lvec= []
     with utils.openw( par['out_f'] ) as outf:
         for r in SeqIO.parse( utils.openr(par['inp_f']), "fastq" if par['q'] else "fasta"):
             l = len(r.seq)
             if par['total']:
                 ltot += l
+            elif par['stat']:
+                lvec.append(l);
             else:
                 outf.write( "\t".join( [r.id,str(l)] ) + "\n" ) 
         if par['total']:
-            outf.write( str(ltot) + "\n" ) 
+            outf.write( str(ltot) + "\n" )
+        if par['stat']:
+            outf.write("number_of_reads\t" + str(len(lvec))  + "\n")
+            outf.write("minimum_read_length\t" + str(np.min(lvec))  + "\n")
+            outf.write("median_read_length\t" + str(np.median(lvec))  + "\n")
+            outf.write("mean_read_length\t" + str(np.mean(lvec)) + "\n")
