4a2536bc5d85305f1468355e9ea0f15981832398
duytin <duytin@localhost>
2015-06-06 23:12:53 +0200

    fastq2fasta.py accepts stdin and stdout

--- fastq2fasta.py
+++ fastq2fasta.py
@@ -3,14 +3,23 @@ from Bio import SeqIO
 import argparse as ap
 import sys
 
-def read_params(args):
-	p = ap.ArgumentParser(description = 'fastq2fasta.py Parameters\n')
-	p.add_argument('--ifn', required = True, default = None, type = str)
-	p.add_argument('--ofn', required = True, default = None, type = str)
-	return vars(p.parse_args())
-	
+def read_params():
+    p = ap.ArgumentParser(description = 'fastq2fasta.py Parameters\n')
+    p.add_argument('--ifn', required = False, default = None, type = str)
+    p.add_argument('--ofn', required = False, default = None, type = str)
+    return vars(p.parse_args())
+
+    
 if __name__ == '__main__':
-	args = read_params(sys.argv)
-	with open(args['ofn'], 'w') as ofile:
-		for r in SeqIO.parse(args['ifn'], "fastq"):
-			SeqIO.write(r, ofile, "fasta")
+    args = read_params()
+    if args['ifn'] == None:
+        ifile = sys.stdin
+    else:
+        ifile = open(args['ifn'], 'r')
+    if args['ofn'] == None:
+        ofile = sys.stdout
+    else:
+        ofile = open(args['ofn'], 'w')
+    
+    for r in SeqIO.parse(ifile, "fastq"):
+        SeqIO.write(r, ofile, "fasta")
