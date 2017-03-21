780d178e38511c32928bae0b4604fcb10174bc50
Francesco Asnicar <francesco.asnicar@gmail.com>
2017-03-21 18:19:08 +0100

    hgignore added

--- fna_len.py
+++ fna_len.py
@@ -1,49 +1,55 @@
-#!/usr/bin/env python 
-from __future__ import with_statement
+#!/usr/bin/env python
 
+
+from __future__ import with_statement
 import sys
 import argparse
-import os
-import textwrap
-from collections import namedtuple as nt
-import random as rnd
-rnd.seed(1982)
 import utils
 from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
 import numpy as np
+import os
 
-def read_params(args):
-    parser = argparse.ArgumentParser(description='Display the length of each fasta entry')
-    arg = parser.add_argument
-    arg( 'inp_f', metavar='INPUT_FILE', nargs='?', default=None, type=str,
-         help="the input fna file [stdin if not present]")
-    arg( 'out_f', metavar='OUTPUT_FILE', nargs='?', default=None, type=str,
-         help="the output txt file [stdout if not present]")
-    arg( '-q', action='store_true', 
-         help="set this for fastq")
-    arg( '-t','--total', action='store_true', help="Print only the sum of the length of all sequences\n")
-    arg( '-s','--stat', action='store_true', help="Print only the statistics about the length of sequences\n")
-    return vars(parser.parse_args())
+
+def read_params():
+    p = argparse.ArgumentParser(description='Display the length of each fasta entry')
+    p.add_argument('inp_f', metavar='INPUT_FILE', nargs='?', default=None, type=str,
+        help="the input fna file [stdin if not present]")
+    p.add_argument('out_f', metavar='OUTPUT_FILE', nargs='?', default=None, type=str,
+        help="the output txt file [stdout if not present]")
+    p.add_argument('-q', action='store_true',
+        help="set this for fastq")
+    p.add_argument('-t', '--total', action='store_true', default=False,
+        help="Print only the sum of the length of all sequences\n")
+    p.add_argument('-s', '--stat', action='store_true', default=False,
+        help="Print only the statistics about the length of sequences\n")
+
+    return vars(p.parse_args())
+
 
 if __name__ == '__main__':
-    par = read_params(sys.argv)
+    par = read_params()
+    lvec = []
+    samplename = None
 
-    ltot = 0
-    lvec= []
-    with utils.openw( par['out_f'] ) as outf:
-        for r in SeqIO.parse( utils.openr(par['inp_f']), "fastq" if par['q'] else "fasta"):
+    if par['inp_f']:
+        samplename = par['inp_f'][par['inp_f'].rfind('/')+1:]
+        samplename = samplename[:samplename.rfind('.')]
+    else:
+        samplename = 'stdin'
+        samplename += '_fastq' if par['q'] else '_fasta'
+
+    with utils.openw(par['out_f']) as outf:
+        for r in SeqIO.parse(utils.openr(par['inp_f']), "fastq" if par['q'] else "fasta"):
             l = len(r.seq)
-            if par['total']:
-                ltot += l
-            elif par['stat']:
-                lvec.append(l);
+
+            if par['stat'] or par['total']:
+                lvec.append(l)
             else:
-                outf.write( "\t".join( [r.id,str(l)] ) + "\n" ) 
+                outf.write("\t".join([r.id, str(l)]) + "\n")
+
         if par['total']:
-            outf.write( str(ltot) + "\n" )
+            outf.write(str(np.sum(lvec)) + "\n")
+
         if par['stat']:
-            outf.write("number_of_reads\t" + str(len(lvec))  + "\n")
-            outf.write("minimum_read_length\t" + str(np.min(lvec))  + "\n")
-            outf.write("median_read_length\t" + str(np.median(lvec))  + "\n")
-            outf.write("mean_read_length\t" + str(np.mean(lvec)) + "\n")
+            outf.write('\t'.join(["#samplename", "n_of_bases", "n_of_reads", "min_read_len", "median_read_len", "mean_read_len", "max_read_len"]) + '\n')
+            outf.write('\t'.join([str(a) for a in [samplename, np.sum(lvec), len(lvec), np.min(lvec), np.median(lvec), np.mean(lvec), np.max(lvec)]]) + '\n')
